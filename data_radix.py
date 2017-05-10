3f2dd5c9c7ef686475824717f72b710fc3251729
Lucas Cerqueira <lucas.cerqueira@poli.ufrj.br>
2017-05-10 01:17:25 -0300

    New pictures | Improved training and network structure

--- data_radix.py
+++ data_radix.py
@@ -1,15 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 # Author: Lucas de Andrade Cerqueira
 # Email: lucas.cerqueira@poli.ufrj.br
 
+import sys  
 
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
 plt.close('all')
-
 num_classes = 2
+
+def plot_temp(temp_y, temp_x, axis=None):
+	if (temp_y < 1) or (temp_y > 4) or (temp_x < 1) or (temp_x > 4):
+		return
+
+	if (not axis):
+		plt.figure()
+		plt.plot (train_data[train_data[:,4] == 0,temp_x-1], train_data[train_data[:,4] == 0,temp_y-1], 'r*', label='Estado regular')
+		plt.plot (train_data[train_data[:,4] == 1,temp_x-1], train_data[train_data[:,4] == 1,temp_y-1], 'bo', label='Estado ótimo')
+		plt.xlabel ('Temperatura '+str(temp_x)+' (°C)', fontsize=14)
+		plt.ylabel ('Temperatura '+str(temp_y)+' (°C)', fontsize=14)
+		plt.title ('Temp'+str(temp_y)+' x Temp'+str(temp_x)+' por classe', fontsize="x-large", fontweight='bold')
+		plt.legend()
+		plt.show()
+	else:
+		print ("Entrou")
+		axis.plot (train_data[train_data[:,4] == 0,temp_x-1], train_data[train_data[:,4] == 0,temp_y-1], 'r*', label='Estado regular')
+		axis.plot (train_data[train_data[:,4] == 1,temp_x-1], train_data[train_data[:,4] == 1,temp_y-1], 'bo', label='Estado ótimo')
+		axis.set_xlabel ('Temperatura '+str(temp_x)+' (°C)', fontsize=14)
+		axis.set_ylabel ('Temperatura '+str(temp_y)+' (°C)', fontsize=14)
+		axis.set_title ('Temp'+str(temp_y)+' x Temp'+str(temp_x)+' por classe', fontsize="x-large", fontweight='bold')
+		axis.legend()
+
+
 
 csvfile = open('p1_data_train.csv', 'r')
 reader = csv.reader(csvfile, delimiter=',')
@@ -17,33 +46,53 @@ train_data = list(reader)
 train_data = np.array(train_data)
 train_data = np.delete (train_data, 0, 0).astype('float')   		# Remove first line that contains the headers
 
-indexes = (train_data[:,4] == 1)
 
-print (indexes)
+# Plot histogram of data for each class
+fig, axarr = plt.subplots(2, sharex=True)
+plt.set_cmap('jet_r')
 
-print (train_data[indexes])
-
-
-plt.plot (train_data[indexes,0], train_data[indexes,1])
-
-# fig, axarr = plt.subplots(2, sharex=True)
-
-# for i in range(num_classes):
-	# indexes = (train_data[:,4] == i)
-	# array = train_data[indexes]
+for i in range(num_classes):
+	indexes = (train_data[:,4] == i)
+	array = train_data[indexes]
+	array = array[:,0:4]
 	
-	# axarr[i].hist(array, bins='auto', label=['Temp. 1', 'Temp. 2', 'Temp. 3', 'Temp. 4'])
-	# axarr[i].legend()
-	# axarr[i].set_ylabel('Nº de ocorrências', fontsize=14)
+	axarr[i].hist(array, bins='auto', label=['Temp. 1', 'Temp. 2', 'Temp. 3', 'Temp. 4'])
+	axarr[i].legend()
+	axarr[i].set_ylabel('Nº de ocorrências', fontsize=14)
+	axarr[i].yaxis.set_ticks(np.arange(0, 500, 50))
 	
-	# if (i==0):
-		# axarr[i].set_title('Estado ótimo', fontweight='bold')
+	if (i==0):
+		axarr[i].set_title('Estado ótimo', fontweight='bold')
 		
-	# else:
-		# axarr[i].set_title('Estado regular', fontweight='bold')
+	else:
+		axarr[i].set_title('Estado regular', fontweight='bold')
 
 
-# axarr[1].set_xlabel('Temperatura (°C)', fontsize=14)
-# fig.suptitle ('Histograma das temperaturas por estado no conjunto de treinamento', fontsize="x-large", fontweight='bold')
+axarr[1].set_xlabel('Temperatura (°C)', fontsize=14)
+fig.suptitle ('Histograma das temperaturas por estado no conjunto de treinamento', fontsize="x-large", fontweight='bold')
+plt.show()
+
+
+# fig, axarr = plt.subplots(1,3)
+
+# for i in range(axarr.shape[0]):
+# 	plot_temp (i+2, 1, axarr[i])	
+
 # plt.show()
+
+
+# fig, axarr = plt.subplots(1,2)
+
+# for i in range(axarr.shape[0]):
+# 	plot_temp (i+3, 2, axarr[i])	
+
+# plt.show()
+
+# plot_temp (4,3)
+
+#Plot Temp4 x Temp2 for each class
+plot_temp(4,2)
+
+#Plot Temp3 x Temp1 for each class
+plot_temp (3,1)
 
--- mlp_radix.py
+++ mlp_radix.py
@@ -1,5 +1,5 @@
 # Author: Lucas de Andrade Cerqueira
-# Email: lucas.cerqueira@poli.ufrj.br
+# Email:  lucas.cerqueira@poli.ufrj.br
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,31 +8,44 @@ import keras
 
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.callbacks import EarlyStopping
+from sklearn.cross_validation import train_test_split
 
 num_classes = 2
-batch_size = 100
-epochs = 100
+batch_size = 50
+epochs = 400
 
 csvfile = open('p1_data_train.csv', 'r')
 reader = csv.reader(csvfile, delimiter=',')
 train_data = list(reader)
 train_data = np.array(train_data)
 train_data = np.delete (train_data, 0, 0).astype('float')   		# Remove first line that contains the headers
+csvfile.close()
 
 
 # Split the array between the inputs and the targets
 input_data = train_data[:,0:4]
 target_data = train_data[:,4]
 
-inputs = Input(shape=(input_data.shape[1],))
+input_train, input_test, target_train, target_test = train_test_split (input_data, target_data, test_size=0.25)
 
-hidden = Dense (10, activation="sigmoid") (inputs)
-hidden = Dense (4, activation="sigmoid") (hidden)
+# 20 5 1 : 350ep
+
+inputs = Input(shape=(input_train.shape[1],))
+hidden = Dense (20, activation="sigmoid") (inputs)
+hidden = Dense (10, activation="sigmoid") (hidden)
 output = Dense (1, activation="sigmoid") (hidden)
 
 model = Model (inputs=inputs, outputs=output)
-model.compile(loss=keras.losses.mean_squared_error, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
 
-model.fit(input_data, target_data, batch_size=batch_size, epochs=epochs, verbose=1)
-score = model.evaluate(input_data, target_data, verbose=0)
+model.compile(loss=keras.losses.mean_squared_error, optimizer=keras.optimizers.Adagrad(), metrics=['accuracy'])
+
+earlyStop = EarlyStopping(monitor='val_acc', patience=25)
+
+history = model.fit(input_train, target_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(input_test,target_test),
+	                callbacks = [earlyStop])
+
+
+score = model.evaluate(input_test, target_test, verbose=0)
+print score
 
